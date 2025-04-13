2a72509cc6f78288b1b35b82e2a0952d6eb765af
hawonjungo <hawon.jungo@gmail.com>
2025-04-13 22:16:15 +1000

    add Logo section and modify fixed className on Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero.jsx";
 import ShowCaseSection from "./sections/ShowCaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import LogoSection from "./components/LogoSection.jsx";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <ShowCaseSection />
+            <LogoSection />
         </>
     )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
                                     <span className="wrapper">
                                         {words.map((word)=>(
                                             <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                                                <img src={word.imgPath} alt={word.text} className="xl:size-12 md size-10  md:p-2 p-1 rounded-full bg-white-50" />
+                                                <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7  md:p-2 p-1 rounded-full bg-white-50" />
                                             <span>{word.text}</span>
                                             </span>
                                         ))}
