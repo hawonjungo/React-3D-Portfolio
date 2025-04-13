9d42456c174f8fa44415320383231c937ddb4412
hawonjungo <hawon.jungo@gmail.com>
2025-04-13 21:39:14 +1000

    add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./sections/Hero.jsx";
 import ShowCaseSection from "./sections/ShowCaseSection.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Hero />
             <ShowCaseSection />
         </>
