4fefa5cd0a4050a97f33a0bd1291db319705a3ce
hawonjungo <hawon.jungo@gmail.com>
2025-05-16 19:30:33 +1000

    adjust techlogo padding

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -27,7 +27,7 @@ const TechStack = () => {
                     title="My Preferred Tech Stack"
                     sub=" 🤝 The Skills I Bring to the Table"
                 />
-                <div className="tech-grid ">
+                <div className="tech-grid md:px-10 px-20">
                     {techStackIcons.map((icon) => (
                         <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
                             <div className="tech-card-animated-bg" />
