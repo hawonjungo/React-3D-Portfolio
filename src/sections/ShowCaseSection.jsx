c5de59f193d385193b77e0685cd6ed3082df366e
hawonjungo <hawon.jungo@gmail.com>
2025-09-06 15:47:14 +1000

    add Respeako project for show case

--- src/sections/ShowCaseSection.jsx
+++ src/sections/ShowCaseSection.jsx
@@ -1,7 +1,7 @@
-import {useRef} from "react";
-import {gsap} from 'gsap';
-import {ScrollTrigger} from "gsap/ScrollTrigger";
-import {useGSAP} from "@gsap/react";
+import { useRef } from "react";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -18,16 +18,18 @@ const ShowCaseSection = () => {
         const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
         projects.forEach((card, index) => {
             gsap.fromTo(card,
-                {y: 50, opacity: 0},
-                {y: 0,opacity :1, duration:1,delay:0.3 *(index+1),scrollTrigger:{
-                        trigger:card,
+                { y: 50, opacity: 0 },
+                {
+                    y: 0, opacity: 1, duration: 1, delay: 0.3 * (index + 1), scrollTrigger: {
+                        trigger: card,
                         start: 'top bottom -=100',
-                    }})
+                    }
+                })
         })
         gsap.fromTo(sectionRef.current,
-            {opacity: 0},
-            {opacity: 1,duration:1.5})
-    },[]);
+            { opacity: 0 },
+            { opacity: 1, duration: 1.5 })
+    }, []);
     return (
 
         <section id="work" ref={sectionRef} className="app-showcase">
@@ -36,17 +38,33 @@ const ShowCaseSection = () => {
                     {/*LEFT*/}
                     <div className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
-                            <img src="/images/3Element.PNG" alt="3Elements"   className="w-full h-full object-contain"/>
+                            <img src="/images/ReSpeako.gif" alt="ReSpeako" className="w-full h-full object-contain" />
                         </div>
                         <div className="text-content">
-                            <h2>C++ Game (SDL2): Titled "Three Elements".</h2>
+                            <h2>ReSpeako</h2>
+                            <a
+                                href="https://respeako.relifes.net/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-400 hover:underline"
+                            >
+                                https://respeako.relifes.net/
+                            </a>
                             <p className="text-white-50 md:text-xl">
-                                2D game using OOP, SDL2 for graphics and event handling.
+                                ReSpeako is a versatile language tool application built with React and Capacitor.
+                                It empowers users with speech-to-text, text-to-speech, and an IPA (International Phonetic Alphabet) lookup feature, aiming to assist with language learning and pronunciation.
+                                The application is designed to be cross-platform, with the current configuration targeting Web and Android.
                             </p>
                         </div>
                     </div>
                     {/*RIGHT*/}
                     <div className="project-list-wrapper overflow-hidden">
+                        <div className="project" ref={project2Ref}>
+                            <div className="image-wrapper bg-[#ffe7eb]">
+                                <img src="/images/3Element.PNG" alt="3Elements" />
+                            </div>
+                            <h2> 2D game using OOP, SDL2 for graphics and event handling. </h2>
+                        </div>
                         <div className="project" ref={project2Ref}>
                             <div className="image-wrapper bg-[#ffefdb]">
                                 <img src="/images/rosagif.gif" alt="rosaPig" />
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
             <div className="w-full h-full md:px-10 px-5">
                 <TitleHeader title="What people say about me?"
                               sub="✨ Client Feedback Highlights"/>
-                <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
+                <div className="columns-1 mt-16">
                     {testimonials.map(({imgPath,name,mentions,review}) => (
                        <GlowCard card={{review}} >
                            <div className="flex items-center gap-3">
